8a69b34b232a8d398c27cd99ce595241a665941b
Patrick Binneveld <patrick.binneveld@hotmail.com>
2019-06-23 16:14:22 +0200

    Form validation
    Added form validation, so fields can't be empty

--- modules/postsForm.php
+++ modules/postsForm.php
@@ -1,9 +1,9 @@
 <form method="post" action="pushpost.php">  
-    <h4>Uw naam: <br /></h4>
-        <input id="formtext" type="text" name="inputTitle" value="<?php echo $inputTitle;?>"> <br />
+    <h4>Uw naam <abbr title="This field is mandatory">*</abbr><br /></h4>
+        <input id="formtext" type="text" name="inputTitle" required name="title" value="<?php echo $inputTitle;?>"> <br />
 
-    <h4>Uw bericht: <br /></h4>
-        <textarea id="formarea" name="inputContent" rows="4" maxlength="255"><?php echo $inputContent;?></textarea>
+    <h4>Uw bericht <abbr title="This field is mandatory">*</abbr><br /></h4>
+        <textarea id="formarea" name="inputContent" required name="title" rows="4" maxlength="255"><?php echo $inputContent;?></textarea>
         <div id="textarea_message"></div><br />
 
     <!-- Honeypot, wordt niet weergegeven en functioneert alleen tegen spam/bots -->
